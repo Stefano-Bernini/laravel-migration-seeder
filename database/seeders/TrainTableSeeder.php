8c463501dfab00301eb87187e2a32f2775e22db9
Stefano-Bernini <stefanobernini26@gmail.com>
2023-08-02 22:04:37 +0200

    creata e salvata nuova istanza classe model nel db

--- database/seeders/TrainTableSeeder.php
+++ database/seeders/TrainTableSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Train;
-use Faker\Generator as Faaker;
+use Faker\Generator as Faker;
 use Faker\Provider\en_US\Person;
 
 class TrainTableSeeder extends Seeder
@@ -15,8 +15,25 @@ class TrainTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i=0; $i<10; $i++){
+            $train = new Train();
+
+            $train->azienda = $faker->word();
+            $train->stazione_partenza = $faker->word();
+            $train->stazione_arrivo = $faker->word();
+            $train->data_partenza = $faker->date();
+            $train->data_arrivo = $faker->date();
+            $train->orario_partenza = $faker->time();
+            $train->orario_arrivo = $faker->time();
+            $train->codice_treno = $faker->bothify('nt???##');
+            $train->n_carrozze = $faker->numberBetween(1,15);
+            $train->in_orario = $faker->boolean();
+            $train->tempo_ritardo = $faker->time();
+            $train->cancellato = $faker->boolean();
+
+            $train->save();
+        }
     }
 }
