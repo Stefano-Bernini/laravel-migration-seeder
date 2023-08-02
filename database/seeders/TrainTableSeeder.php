ad9803141b85e3e8f705f6c3fc5a926f2430a476
Stefano-Bernini <stefanobernini26@gmail.com>
2023-08-02 21:21:43 +0200

    creato e importato Faker

--- database/seeders/TrainTableSeeder.php
+++ database/seeders/TrainTableSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Train;
+use Faker\Generator as Faaker;
+use Faker\Provider\en_US\Person;
 
 class TrainTableSeeder extends Seeder
 {
